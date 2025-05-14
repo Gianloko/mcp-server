5a3d0faced4d9bd5fae306ab13c175c0564501a2
Gianluca Tessitore <gian.tessitore@gmail.com>
2025-05-14 10:34:42 +0200

    Update mcp-agent.py

--- mcp-agent.py
+++ mcp-agent.py
@@ -19,20 +19,25 @@ async def run(mcp_server: MCPServer):
     )
 
     # Use the `add` tool to add two numbers
-    message = "Give me some Leads"
-    print(f"Running: {message}")
+    message = """
+                Execute these actions in order: 
+                    0) Read 10 Leads
+                    1) Update lead 00QgL00000020fQUAQ – set LastName to "MCP Agent Action"
+              """
+
+    print(f"Initial user prompt: {message}")
     result = await Runner.run(starting_agent=agent, input=message)
-    print(result.final_output)
+    print(f"Result: {result.final_output}")
 
 async def main():
     async with MCPServerSse(
-        name="SSE Python Server",
+        name="Salesforce MCP Server",
         params={
-            "url": "http://127.0.0.1:8000/sse",
+            "url": "http://127.0.0.1:8001/sse",
         },
     ) as server:
         trace_id = gen_trace_id()
-        with trace(workflow_name="SSE Example", trace_id=trace_id):
+        with trace(workflow_name="Salesforce MCP Server", trace_id=trace_id):
             print(f"View trace: https://platform.openai.com/traces/trace?trace_id={trace_id}\n")
             await run(server)
 
@@ -45,13 +50,11 @@ if __name__ == "__main__":
         )
 
     # We'll run the SSE server in a subprocess. Usually this would be a remote server, but for this
-    # demo, we'll run it locally at http://127.0.0.1:8000/sse
+    # demo, we'll run it locally at http://127.0.0.1:8001/sse
     process: subprocess.Popen[Any] | None = None
     try:
         this_dir = os.path.dirname(os.path.abspath(__file__))
         server_file = os.path.join(this_dir, "mcp-server.py")
-
-        print("Starting SSE server at http://127.0.0.1:8000/sse ...")
 
         # Run `uv run server.py` to start the SSE server
         process = subprocess.Popen(["uv", "run", server_file])
